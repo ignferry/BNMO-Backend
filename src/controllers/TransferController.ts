d4046e9993d28105dc6a8be405ce5f7c870db575
Ignasius Ferry <ignasius.ferry01@gmail.com>
2022-08-20 23:43:59 +0700

    Transfer routes

--- src/controllers/TransferController.ts
+++ src/controllers/TransferController.ts
@@ -1,4 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+import { Transaction } from "../db/models/Transaction";
+import { User } from "../db/models/User";
+import { TransactionCreationDto } from "../dtos/TransactionDto";
+import { HttpException } from "../exceptions/HttpException";
 import TransferService from "../services/TransferService";
 
 
@@ -6,10 +10,34 @@ export default class TransferController {
     public transferService = new TransferService();
 
     public getAllTransfers = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const currentUser = res.locals.user as User;
+            if (!currentUser) throw 0;
+            if (!currentUser.is_admin) throw new HttpException(403, "Forbidden");
 
+            const page: number = req.query.page as unknown as number ? req.query.page as unknown as number : 1; 
+            const pageSize: number = req.query.pagesize as unknown as number ? req.query.pagesize as unknown as number : 5;
+
+            const transfers: Transaction[] = await this.transferService.findAllTransfers(page, pageSize);
+
+            res.status(200).json(transfers);
+        } catch (err) {
+            next(err);
+        }
     }
 
     public createTransfer = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            if (!res.locals.user) throw 0;
 
+            const transferData: TransactionCreationDto = req.body;
+            const currentUser: User = res.locals.user as User;
+
+            await this.transferService.createTransfer(transferData, currentUser);
+
+            res.status(201).json({ message: "Transfer created successfully" });
+        } catch (err) {
+            next(err);
+        }
     }
 }
--- src/services/TransferService.ts
+++ src/services/TransferService.ts
@@ -1,11 +1,29 @@
+import lodash from "lodash";
+import { Transaction } from "../db/models/Transaction";
+import { User } from "../db/models/User";
+import { TransactionCreationDto } from "../dtos/TransactionDto";
+import { HttpException } from "../exceptions/HttpException";
 
 
 export default class TransferService {
-    public async findAllTransfers() {
+    public async findAllTransfers(page: number, pageSize: number) {
+        const allRequests: Transaction[] = await Transaction.findAll({
+            limit: pageSize,
+            offset: page * pageSize,
+            where: {
+                type: 0
+            }
+        });
 
+        return allRequests;
     }
 
-    public async createTransfer() {
-        
+    public async createTransfer(transfer: TransactionCreationDto, currentUser: User) {
+        if (lodash.isEmpty(transfer)) throw new HttpException(400, "Request data is empty");
+        if (!(transfer.sender_id && transfer.receiver_id)) throw new HttpException(400, "Sender and receiver id cannot be empty");
+        if (transfer.sender_id == transfer.receiver_id) throw new HttpException(400, "sender and receiver cannot be the same");
+        if (transfer.sender_id != currentUser.id) throw new HttpException(403, "Forbidden");
+
+        await Transaction.create({ ...transfer, type: 1, status: 1 })
     }
 }
