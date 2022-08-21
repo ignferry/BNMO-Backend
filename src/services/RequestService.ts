c2f604fe5cedd7103eaacb511b7e9083fd19603e
Ignasius Ferry <ignasius.ferry01@gmail.com>
2022-08-21 20:23:17 +0700

    Database transaction

--- src/services/RequestService.ts
+++ src/services/RequestService.ts
@@ -3,6 +3,7 @@ import { User } from "../db/models/User";
 import { TransactionCreationDto } from "../dtos/TransactionDto";
 import lodash from "lodash";
 import { HttpException } from "../exceptions/HttpException";
+import { sequelizeConnection } from "../config/default";
 
 
 export default class RequestService {
@@ -45,15 +46,52 @@ export default class RequestService {
     }
 
     public async verifyRequest(requestId: number, status: number) {
-        const findRequest: Transaction | null = await Transaction.findOne({
+        const request: Transaction | null = await Transaction.findOne({
             where: {
                 id: requestId,
                 type: 0
             }
         });
-        if (!findRequest) throw new HttpException(404, "Request not found");
-        if (findRequest.status != 0) throw new HttpException(405, "Request has been processed before");
+        if (!request) throw new HttpException(404, "Request not found");
+        if (request.status != 0) throw new HttpException(405, "Request has been processed before");
 
-        await Transaction.update({ status: status }, { where: { id: requestId } });
+        let errorMessage = "";
+
+        if (status == 1) {
+            // Request accepted
+            try {
+                await sequelizeConnection.transaction(async (t) => {
+                    if (!request.receiver_id) {
+                        // Request to subtract balance
+                        errorMessage = "Sender with the corresponding Id not found";
+                        const sender: User | null = await User.findByPk(request.sender_id, { transaction: t });
+                        if (sender?.is_admin || !sender?.is_verified) throw 0;
+    
+                        errorMessage = "Not enough balance";
+                        const senderBalance = sender?.balance;
+                        if (!senderBalance) throw 0;
+                        if (senderBalance < request.amount) throw 0;
+                        await User.update({ balance: senderBalance-request.amount }, { where: {id: request.sender_id}, transaction: t });
+                    } else {
+                        // Request to add balance
+                        errorMessage = "Receiver with the corresponding Id not found";
+                        const receiver: User | null = await User.findByPk(request.receiver_id, { transaction: t });
+                        if (receiver?.is_admin || !receiver?.is_verified) throw 0;
+    
+                        let receiverBalance = receiver.balance;
+                        if (!receiverBalance) receiverBalance = 0;
+                        await User.update({ balance: receiverBalance+request.amount }, { where: { id: receiver.id }, transaction: t });
+                    }
+
+                    Transaction.update({ status: 1 }, { where: { id: requestId } });
+                });
+            } catch {
+                throw new HttpException(409, errorMessage);
+            }
+        }
+        else {
+            // Request declined
+            await Transaction.update({ status: -1 }, { where: { id: requestId } });
+        }
     }
 }
--- src/services/TransferService.ts
+++ src/services/TransferService.ts
@@ -1,4 +1,5 @@
 import lodash from "lodash";
+import { sequelizeConnection } from "../config/default";
 import { Transaction } from "../db/models/Transaction";
 import { User } from "../db/models/User";
 import { TransactionCreationDto } from "../dtos/TransactionDto";
@@ -21,9 +22,36 @@ export default class TransferService {
     public async createTransfer(transfer: TransactionCreationDto, currentUser: User) {
         if (lodash.isEmpty(transfer)) throw new HttpException(400, "Request data is empty");
         if (!(transfer.sender_id && transfer.receiver_id)) throw new HttpException(400, "Sender and receiver id cannot be empty");
-        if (transfer.sender_id == transfer.receiver_id) throw new HttpException(400, "sender and receiver cannot be the same");
+        if (transfer.sender_id == transfer.receiver_id) throw new HttpException(400, "Sender and receiver cannot be the same");
         if (transfer.sender_id != currentUser.id) throw new HttpException(403, "Forbidden");
 
-        await Transaction.create({ ...transfer, type: 1, status: 1 })
+        let errorMessage = "";
+
+        try {
+            await sequelizeConnection.transaction(async (t) => {
+                errorMessage = "Sender with the corresponding Id not found";
+                const sender: User | null = await User.findByPk(transfer.sender_id, { transaction: t });
+                if (sender?.is_admin || !sender?.is_verified) throw 0;
+    
+                errorMessage = "Receiver with the corresponding Id not found";
+                const receiver: User | null = await User.findByPk(transfer.receiver_id, { transaction: t });
+                if (receiver?.is_admin || !receiver?.is_verified) throw 0;
+
+                errorMessage = "Not enough balance";
+                const senderBalance = sender?.balance;
+                if (!senderBalance) throw 0;
+                if (senderBalance < transfer.amount) throw 0;
+                await User.update({ balance: senderBalance-transfer.amount }, { where: {id: transfer.sender_id} , transaction: t});
+
+                errorMessage = "Transfer error";
+                let receiverBalance = receiver?.balance;
+                if (!receiverBalance) receiverBalance = 0;
+                await User.update({ balance: receiverBalance+transfer.amount }, { where: { id: transfer.receiver_id }, transaction: t });
+
+                await Transaction.create({ ...transfer, type: 1, status: 1 });
+            });
+        } catch {
+            throw new HttpException(409, errorMessage);
+        }
     }
 }
